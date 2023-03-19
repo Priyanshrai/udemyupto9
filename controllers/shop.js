740524f7f575c92d995bb12f3e6a212037eb5cd4
Priyanshrai <rai.priyansh007@gmail.com>
2023-03-20 01:14:05 +0530

    second

--- controllers/shop.js
+++ controllers/shop.js
@@ -11,16 +11,15 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
-exports.getProduct = (req,res,next)=>{
+exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId,product =>{
-   res.render('shop/product-detail',{
-    product:product,
-    pageTitle:product.title,
-    path:'/products'
-   });
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    });
   });
-  
 };
 
 exports.getIndex = (req, res, next) => {
@@ -41,12 +40,12 @@ exports.getCart = (req, res, next) => {
 };
 
 exports.postCart = (req, res, next) => {
-  const prodId=req.body.productId;
-  Product.findById(prodId,(product)=> {
-    Cart.addProduct(prodId,product.price);
-  })
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId, product.price);
+  });
   res.redirect('/cart');
-}
+};
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
